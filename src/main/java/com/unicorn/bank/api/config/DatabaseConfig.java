ada404311a6ec7a29bbcba0fadc09d12ab35fdb3
torranca <torranca@icloud.com>
2022-02-15 21:07:48 +0000

    heroku updates

---
+++ src/main/java/com/unicorn/bank/api/config/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package com.unicorn.bank.api.config;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  @Bean
+  public DataSource dataSource() {
+      HikariConfig config = new HikariConfig();
+      config.setJdbcUrl(dbUrl);
+      return new HikariDataSource(config);
+  }
+}
