fd0bd97b5627a9ece9bc911ff62c9aad91de7d12
torranca <torranca@icloud.com>
2022-02-22 06:56:22 +0000

    cors

--- src/main/java/com/unicorn/bank/UnicornApiApplication.java
+++ src/main/java/com/unicorn/bank/UnicornApiApplication.java
@@ -2,7 +2,10 @@ package com.unicorn.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.unicorn.bank.api.dao")
@@ -12,4 +15,14 @@ public class UnicornApiApplication {
 		SpringApplication.run(UnicornApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:8080","https://x.thunkable.com", "https://thunkable.site");
+			}
+		};
+	}
+	
 }
